ed7456262b073864ad9aac19b14de6c9be0399d5
jonathan_fambro <startek591@gmail.com>
2025-01-13 16:54:18 -0500

    TypeScript union Type

---
+++ 12_typescript_union_type/app.ts
@@ -0,0 +1,15 @@
+function add(
+  a: number | string,
+  b: number | string
+): number | string {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  }
+  if (typeof a === 'string' && typeof b === 'string') {
+    return a.concat(b);
+  }
+  throw new Error('Parameters must be numbers or strings');
+}
+
+console.log(add(1, 2)); // 3
+console.log(add('hello', ' world'));
