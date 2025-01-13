e6fcf92fd0b8d692b0f78ca484bebc18cb789d66
jonathan_fambro <startek591@gmail.com>
2025-01-13 12:46:57 -0500

    TypeScript Number

---
+++ 3_typescript_number/app.js
@@ -0,0 +1,15 @@
+// The number type
+var price;
+// Decimal numbers
+var counter1 = 0;
+var x1 = 100;
+var y1 = 200;
+// Binary Numbers
+var bin = 10;
+var anotherBin = 10;
+// Octal Numbers
+var octal = 8;
+// Hexadecimal Numbers
+var hexadecimal = 0xa;
+// Big Integers
+// let big: bigint = 9007199254740991n;
---
+++ 3_typescript_number/app.ts
@@ -0,0 +1,20 @@
+// The number type
+let price: number;
+
+// Decimal numbers
+let counter1: number = 0;
+let x1: number = 100;
+let y1: number = 200;
+
+// Binary Numbers
+let bin = 0b1010;
+let anotherBin: number = 0b1010;
+
+// Octal Numbers
+let octal: number = 0o10;
+
+// Hexadecimal Numbers
+let hexadecimal: number = 0xa;
+
+// Big Integers
+// let big: bigint = 9007199254740991n;
