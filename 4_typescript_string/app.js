3ac55f73d38c81707ece3de376e106a1d6cc5bda
jonathan_fambro <startek591@gmail.com>
2025-01-13 13:27:13 -0500

    TypeScprit String

---
+++ 4_typescript_string/app.js
@@ -0,0 +1,6 @@
+var firstName = 'John';
+var title = 'Web Developer';
+var firstName2 = "John";
+var title2 = "Web Developer";
+var profile = "I'm ".concat(firstName2, ".\nI'm a ").concat(title2, ".");
+console.log(profile);
---
+++ 4_typescript_string/app.ts
@@ -0,0 +1,9 @@
+let firstName: string = 'John';
+let title: string = 'Web Developer';
+
+let firstName2: string = `John`;
+let title2: string = `Web Developer`;
+let profile: string = `I'm ${firstName2}.
+I'm a ${title2}.`;
+
+console.log(profile);
