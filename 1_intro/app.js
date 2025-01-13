7578bc3ccedbcebf285b942e4d72ea878c4d66b5
jonathan_fambro <startek591@gmail.com>
2025-01-13 11:20:08 -0500

    TypeScript Hello World program in Web Browsers

--- 1_intro/app.js
+++ 1_intro/app.js
@@ -1,2 +1,5 @@
 var message = 'Hello, World!';
 console.log(message);
+var heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
--- 1_intro/app.ts
+++ 1_intro/app.ts
@@ -1,2 +1,7 @@
 let message: string = 'Hello, World!';
 console.log(message);
+
+let heading = document.createElement('h1');
+heading.textContent = message;
+
+document.body.appendChild(heading);
