62fb6ff4deaf5b38c22c908700efb38b12d4708e
jonathan_fambro <startek591@gmail.com>
2025-01-13 14:21:00 -0500

    TypeScript Tuple

---
+++ 8_typescript_tuple/app.js
@@ -0,0 +1,10 @@
+var skill;
+skill = ['Programming', 5];
+console.log(skill); // Output: [ 'Programming', 5 ]
+var color = [255, 0, 0];
+console.log(color);
+var bgColor, headerColor;
+bgColor = [255, 255, 255];
+headerColor = [0, 255, 255];
+console.log(bgColor);
+console.log(headerColor);
---
+++ 8_typescript_tuple/app.ts
@@ -0,0 +1,13 @@
+let skill: [string, number];
+skill = ['Programming', 5];
+
+console.log(skill); // Output: [ 'Programming', 5 ]
+
+let color: [number, number, number] = [255, 0, 0];
+console.log(color);
+
+let bgColor, headerColor: [number, number, number, number?];
+bgColor = [255, 255, 255];
+headerColor = [0, 255, 255];
+console.log(bgColor);
+console.log(headerColor);
